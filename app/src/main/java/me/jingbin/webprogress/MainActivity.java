87863234a04378e3b0c9729c62e331fc33e7b55f
jingbin <770413277@qq.com>
2019-09-17 10:21:08 +0800

    update

--- app/src/main/java/me/jingbin/webprogress/MainActivity.java
+++ app/src/main/java/me/jingbin/webprogress/MainActivity.java
@@ -16,13 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WebProgress mProgress = findViewById(R.id.progress);
-        final WebProgress progressShader = findViewById(R.id.progressShader);
+        final WebProgress mProgressShader = findViewById(R.id.progressShader);
         final WebProgress mProgress2 = findViewById(R.id.progress2);
-        final WebProgress progressShader2 = findViewById(R.id.progressShader2);
+        final WebProgress mProgressShader2 = findViewById(R.id.progressShader2);
         final WebProgress mProgress3 = findViewById(R.id.progress3);
-        final WebProgress progressShader3 = findViewById(R.id.progressShader3);
+        final WebProgress mProgressShader3 = findViewById(R.id.progressShader3);
         final WebProgress mProgress4 = findViewById(R.id.progress4);
-        final WebProgress progressShader4 = findViewById(R.id.progressShader4);
+        final WebProgress mProgressShader4 = findViewById(R.id.progressShader4);
 
         mProgress.setColor(getCompatColor(R.color.colorAccent));
         mProgress.show();
@@ -32,14 +32,15 @@ public class MainActivity extends AppCompatActivity {
                 mProgress.hide();
             }
         }, 2000);
-        progressShader.setColor(getCompatColor(R.color.colorAccentTranslate), getCompatColor(R.color.colorAccent));
-        progressShader.show();
-        progressShader.postDelayed(new Runnable() {
+        mProgressShader.setColor(getCompatColor(R.color.colorAccentTranslate), getCompatColor(R.color.colorAccent));
+        mProgressShader.show();
+        mProgressShader.postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressShader.hide();
+                mProgressShader.hide();
             }
         }, 2000);
+
 
         mProgress2.show();
         mProgress2.postDelayed(new Runnable() {
@@ -48,14 +49,15 @@ public class MainActivity extends AppCompatActivity {
                 mProgress2.hide();
             }
         }, 4000);
-        progressShader2.show();
-        progressShader2.setColor("#002483D9", "#2483D9");
-        progressShader2.postDelayed(new Runnable() {
+        mProgressShader2.show();
+        mProgressShader2.setColor("#002483D9", "#2483D9");
+        mProgressShader2.postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressShader2.hide();
+                mProgressShader2.hide();
             }
         }, 4000);
+
 
         mProgress3.setColor("#1aad19");
         mProgress3.show();
@@ -65,14 +67,15 @@ public class MainActivity extends AppCompatActivity {
                 mProgress3.hide();
             }
         }, 6000);
-        progressShader3.setColor("#001aad19", "#1aad19");
-        progressShader3.show();
-        progressShader3.postDelayed(new Runnable() {
+        mProgressShader3.setColor("#001aad19", "#1aad19");
+        mProgressShader3.show();
+        mProgressShader3.postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressShader3.hide();
+                mProgressShader3.hide();
             }
         }, 6000);
+
 
         mProgress4.show();
         mProgress4.postDelayed(new Runnable() {
@@ -81,12 +84,12 @@ public class MainActivity extends AppCompatActivity {
                 mProgress4.hide();
             }
         }, 10000);
-        progressShader4.show();
-        progressShader4.setColor("#002483D9", "#2483D9");
-        progressShader4.postDelayed(new Runnable() {
+        mProgressShader4.show();
+        mProgressShader4.setColor("#002483D9", "#2483D9");
+        mProgressShader4.postDelayed(new Runnable() {
             @Override
             public void run() {
-                progressShader4.hide();
+                mProgressShader4.hide();
             }
         }, 10000);
     }
